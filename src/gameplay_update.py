b76d17146edeaa33443f065477334d1c00110f24
Bob <bobrobynvw@proton.me>
2025-11-15 15:55:55 +0000

    Added prototype of gameplay_update.py

---
+++ src/gameplay_update.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+
+class GameplayUpdate:
+    def __init__(self):
+        """
+
+        """
+
+    def OthelloUpdate(self, board):
+        """
+        Updates board state for othello rules
+        :return:
+        """
+
+
+
+
+
+    def GrowthUpdate(self, board):
+        """
+        Updates board state for growth rules
+        :return:
+        """
