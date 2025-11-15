26deaaaec921814ac589ab092f15f8d4eeaa53da
Dave-Fish <davefish444@gmail.com>
2025-11-15 22:37:07 +0000

    hopefully fixed othello logic

--- src/gameplay_update.py
+++ src/gameplay_update.py
@@ -19,40 +19,40 @@ class GameplayUpdate:
         Updates board state for othello rules
         :return:
         """
-        board_width = Board.default_size[1]
-        board_height = Board.default_size[0]
+        board_width = Board.default_size[0]
+        board_height = Board.default_size[1]
         # remember to update these if I find something that makes that easy
 
 
-        for cell in board.contents:
-            # making 8 lists of the directions going away from the piece that's going to do the othelloing
-            x_before =[]
-            xy_before_above = []
-            y_above = []
-            xy_after_above = []
-            x_after = []
-            xy_after_below = []
-            y_below = []
-            xy_before_below = []
-            directions = [x_before, xy_before_above, y_above, xy_after_above, x_after, xy_after_below, y_below, xy_before_below]
-            for i in range (x):
-                x_before.append(board.contents[i,y])
-                x_before.reverse()
-            for i in range (min(x, y)):
-                xy_before_above.append(board.contents[x-i-1, y-i-1])
-            for i in range (y):
-                y_above.append(board.contents[x,i])
-                y_above.reverse()
-            for i in range (min(board_width-x, y)-1):
-                xy_after_above.append(board.contents[x+i+1, y-i-1])
-            for i in range (board_width-x-1):
-                x_after.append(board.contents[x+i+1,y])
-            for i in range (min(board_width-x, board_height-y)-1):
-                xy_after_below.append(board.contents[x+i+1, y+i+1])
-            for i in range (board_height-y-1):
-                y_below.append(board.contents[x,y+i+1])
-            for i in range (min(x, board_height-y)-1):
-                xy_before_below.append(board.contents[x-i-1, y+i+1])
+        
+        # making 8 lists of the directions going away from the piece that's going to do the othelloing
+        x_before =[]
+        xy_before_above = []
+        y_above = []
+        xy_after_above = []
+        x_after = []
+        xy_after_below = []
+        y_below = []
+        xy_before_below = []
+        directions = [x_before, xy_before_above, y_above, xy_after_above, x_after, xy_after_below, y_below, xy_before_below]
+        for i in range (x):
+            x_before.append(board.contents[i,y])
+            x_before.reverse()
+        for i in range (min(x, y)):
+            xy_before_above.append(board.contents[x-i-1, y-i-1])
+        for i in range (y):
+            y_above.append(board.contents[x,i])
+            y_above.reverse()
+        for i in range (min(board_width-x-1, y)):
+            xy_after_above.append(board.contents[x+i+1, y-i-1])
+        for i in range (board_width-x-1):
+            x_after.append(board.contents[x+i+1,y])
+        for i in range (min(board_width-x-1, board_height-y-1)):
+            xy_after_below.append(board.contents[x+i+1, y+i+1])
+        for i in range (board_height-y-1):
+            y_below.append(board.contents[x,y+i+1])
+        for i in range (min(x, board_height-y-1)):
+            xy_before_below.append(board.contents[x-i-1, y+i+1])
 
 
 
