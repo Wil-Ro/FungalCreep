fd732ec6db6eb9ef5a7d03649570b26a3e12afd9
Dave-Fish <davefish444@gmail.com>
2025-11-16 16:40:16 +0000

    fixed indenting

--- src/gameplay_update.py
+++ src/gameplay_update.py
@@ -29,45 +29,42 @@ class GameplayUpdate:
 
         
         # making 8 lists of the directions going away from the piece that's going to do the othelloing
-        try:
-            x_before = []
-            logging.debug("x_before: %s", x_before)
-            xy_before_above = []
-            logging.debug("xy_before_above: %s", xy_before_above)
-            y_above = []
-            logging.debug("y_above: %s", y_above)
-            xy_after_above = []
-            logging.debug("xy_after_above: %s", xy_after_above)
-            x_after = []
-            logging.debug("x_after: %s", x_after)
-            xy_after_below = []
-            logging.debug("xy_after_below: %s", xy_after_below)
-            y_below = []
-            logging.debug("y_below: %s", y_below)
-            xy_before_below = []
-            logging.debug("xy_before_below: %s", xy_before_below)
-            directions = [x_before, xy_before_above, y_above, xy_after_above, x_after, xy_after_below, y_below, xy_before_below]
-            for i in range (x):
-                x_before.append([i,y])
-                x_before.reverse()
-            for i in range (min(x, y)):
-                xy_before_above.append([x-i-1, y-i-1])
-            for i in range (y):
-                y_above.append([x,i])
-                y_above.reverse()
-            for i in range (min(board_width-x-1, y)):
-                xy_after_above.append([x+i+1, y-i-1])
-            for i in range (board_width-x-1):
-                x_after.append([x+i+1,y])
-            for i in range (min(board_width-x-1, board_height-y-1)):
-                xy_after_below.append([x+i+1, y+i+1])
-            for i in range (board_height-y-1):
-                y_below.append([x,y+i+1])
-            for i in range (min(x, board_height-y-1)):
-                xy_before_below.append([x-i-1, y+i+1])
-            logging.debug("directions: %s", directions)
-        except:
-            logging.debug("whoopsie I crashed")
+        x_before = []
+        logging.debug("x_before: %s", x_before)
+        xy_before_above = []
+        logging.debug("xy_before_above: %s", xy_before_above)
+        y_above = []
+        logging.debug("y_above: %s", y_above)
+        xy_after_above = []
+        logging.debug("xy_after_above: %s", xy_after_above)
+        x_after = []
+        logging.debug("x_after: %s", x_after)
+        xy_after_below = []
+        logging.debug("xy_after_below: %s", xy_after_below)
+        y_below = []
+        logging.debug("y_below: %s", y_below)
+        xy_before_below = []
+        logging.debug("xy_before_below: %s", xy_before_below)
+        directions = [x_before, xy_before_above, y_above, xy_after_above, x_after, xy_after_below, y_below, xy_before_below]
+        for i in range (x):
+            x_before.append([i,y])
+            x_before.reverse()
+        for i in range (min(x, y)):
+            xy_before_above.append([x-i-1, y-i-1])
+        for i in range (y):
+            y_above.append([x,i])
+            y_above.reverse()
+        for i in range (min(board_width-x-1, y)):
+            xy_after_above.append([x+i+1, y-i-1])
+        for i in range (board_width-x-1):
+            x_after.append([x+i+1,y])
+        for i in range (min(board_width-x-1, board_height-y-1)):
+            xy_after_below.append([x+i+1, y+i+1])
+        for i in range (board_height-y-1):
+            y_below.append([x,y+i+1])
+        for i in range (min(x, board_height-y-1)):
+            xy_before_below.append([x-i-1, y+i+1])
+        # logging.debug("directions: %s", directions)
 
 
 
